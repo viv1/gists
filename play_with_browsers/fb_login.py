2f4b61fe858ca0e03b113a527dc1b0810a8c82cf
vivek <f2012058@pilani.bits-pilani.ac.in>
2015-01-22 17:28:26 +0530

    Lines added to search a keyword in facebook Search(not working properly

--- play_with_browsers/fb_login.py
+++ play_with_browsers/fb_login.py
@@ -1,4 +1,9 @@
 # Simple script to login to facebook
+# Supports only python2 
+# just change raw_input to input and it will support python3 also.
+
+# Requires selenium to be installed....
+# sudo python2 -m pip install selenium (or sudo python3 -m pip install selenium)
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -16,4 +21,10 @@ password=driver.find_element_by_name("pass")
 password.send_keys(pwd)
 
 driver.find_element_by_id("loginbutton").click()
+
+el=driver.find_element_by_class_name("_5861 textInput _586g _586p focus_target")
+
+el.send_keys("Sahu")
+el.send_keys(Keys.RETURN)
+
 
