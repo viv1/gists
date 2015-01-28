22f85e5dafc4b0bf919cb440f925dbc226c7bdd9
vivek <f2012058@pilani.bits-pilani.ac.in>
2015-01-28 16:50:40 +0530

    to do added for irctc script

--- Tatkal_script/Tatkal_script.py
+++ Tatkal_script/Tatkal_script.py
@@ -3,8 +3,18 @@
 #This script will book Tatkal tickets for you
 #You will have to provide a few details before_hand
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.irctc.com")
+
+
 """
 
 TO DO
+
+1.Use find element to find login and pwd boxes and fill it up.
+2.Use pytesseract to extract text from captcha
 
 """
