2296a1203e48ce86b115f42fa60586f043523a3e
vivek <f2012058@pilani.bits-pilani.ac.in>
2015-02-16 13:52:06 +0530

    Can extract the image from site, but needs to be trained to get proper image_to_text  result

--- Tatkal_script/Tatkal_script.py
+++ Tatkal_script/Tatkal_script.py
@@ -7,8 +7,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("http://www.irctc.com")
+driver.get("https://www.irctc.co.in/eticketing/loginHome.jsf")
+driver.save_screenshot('irctc.png')
+driver.find_element_by_class_name("loginUserId").send_keys("bhuwanlals")
+driver.find_element_by_class_name("loginPassword").send_keys("bep149")
 
+
+driver.quit()
 
 """
 
